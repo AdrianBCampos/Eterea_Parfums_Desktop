8fbcb2737a3a67e65d53431163a0d3e38fe35375
AdrianBCampos <adrian.campos@davinci.edu.ar>
2025-01-06 19:40:15 -0300

    Armando Form Login

--- Eterea_Parfums_Desktop/Login.Designer.cs
+++ Eterea_Parfums_Desktop/Login.Designer.cs
@@ -28,12 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtUsuario = new System.Windows.Forms.Label();
+            this.txtContraseña = new System.Windows.Forms.Label();
+            this.inputUsuario = new System.Windows.Forms.TextBox();
+            this.inputContraseña = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.AutoSize = true;
+            this.txtUsuario.Location = new System.Drawing.Point(128, 103);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(64, 20);
+            this.txtUsuario.TabIndex = 0;
+            this.txtUsuario.Text = "Usuario";
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.AutoSize = true;
+            this.txtContraseña.Location = new System.Drawing.Point(128, 185);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(92, 20);
+            this.txtContraseña.TabIndex = 1;
+            this.txtContraseña.Text = "Contraseña";
+            // 
+            // inputUsuario
+            // 
+            this.inputUsuario.Location = new System.Drawing.Point(300, 97);
+            this.inputUsuario.Name = "inputUsuario";
+            this.inputUsuario.Size = new System.Drawing.Size(177, 26);
+            this.inputUsuario.TabIndex = 2;
+            // 
+            // inputContraseña
+            // 
+            this.inputContraseña.Location = new System.Drawing.Point(300, 179);
+            this.inputContraseña.Name = "inputContraseña";
+            this.inputContraseña.Size = new System.Drawing.Size(177, 26);
+            this.inputContraseña.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(285, 306);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(189, 47);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Ingresar";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.inputContraseña);
+            this.Controls.Add(this.inputUsuario);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.txtUsuario);
+            this.Name = "Login";
             this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label txtUsuario;
+        private System.Windows.Forms.Label txtContraseña;
+        private System.Windows.Forms.TextBox inputUsuario;
+        private System.Windows.Forms.TextBox inputContraseña;
+        private System.Windows.Forms.Button btnLogin;
     }
 }
