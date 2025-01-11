307307bae0e4358d69cbc123f5c121591ca0760d
DamiMorlan <damian.morlan@davinci.edu.ar>
2025-01-11 15:45:09 -0300

    Wip rolempleado. modifique modelo empleado

--- Eterea_Parfums_Desktop/Modelos/Empleado.cs
+++ Eterea_Parfums_Desktop/Modelos/Empleado.cs
@@ -13,10 +13,27 @@ namespace Eterea_Parfums_Desktop.Modelos
         public String clave { get; set; }
         public String nombre { get; set; }
         public String apellido { get; set; }
+        public int dni { get; set; }
+        public DateTime fecha_nacimiento { get; set; }
+        public String celular { get; set; }
+        public String e_mail { get; set; }
+        public int pais_id { get; set; }
+        public int provincia_id { get; set; }
+        public int localidad_id { get; set; }
+        public int codigo_postal { get; set; }
+        public int calle_id { get; set; }
+        public int numeracion_calle { get; set; }
+        public String piso { get; set; }
+        public String departamento { get; set; }
+        public String comentarios_domicilio { get; set; }
+        public int sucursal_id { get; set; }
+        public DateTime fecha_ingreso { get; set; }
+        public int sueldo { get; set; }
+        public int activo { get; set; }
         public String rol { get; set; }
-    
 
-     public Empleado(int id, string usuario, string clave, string nombre, string apellido, string rol)
+
+        public Empleado(int id, string usuario, string clave, string nombre, string apellido, string rol)
         {
             this.id = id;
             this.usuario = usuario;
