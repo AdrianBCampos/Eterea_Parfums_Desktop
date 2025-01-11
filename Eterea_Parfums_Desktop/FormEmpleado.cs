a2bce831e1daca5fe328f3848db21087248d31bc
DamiMorlan <damian.morlan@davinci.edu.ar>
2025-01-11 15:51:39 -0300

    formEmpleado modificado

---
+++ Eterea_Parfums_Desktop/FormEmpleado.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Eterea_Parfums_Desktop
+{
+    public partial class FormEmpleado : Form
+    {
+        public FormEmpleado()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
