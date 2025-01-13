ab0e2ca3858d8cb62b0534c9a0a16318e391c602
joseluisx10 <joseluis.quispe_91@hotmail.com>
2025-01-13 00:37:16 -0300

    creando form MenuABM

---
+++ Eterea_Parfums_Desktop/MenuABM.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Eterea_Parfums_Desktop
+{
+    partial class MenuABM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel_abm = new System.Windows.Forms.Panel();
+            this.btn_promociones = new System.Windows.Forms.Button();
+            this.btn_clientes = new System.Windows.Forms.Button();
+            this.btn_empleados = new System.Windows.Forms.Button();
+            this.btn_perfumes = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(112)))), ((int)(((byte)(112)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(959, 39);
+            this.button2.Name = "button2";
+            this.button2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button2.Size = new System.Drawing.Size(42, 41);
+            this.button2.TabIndex = 110;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // panel_abm
+            // 
+            this.panel_abm.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel_abm.AutoSize = true;
+            this.panel_abm.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.panel_abm.Location = new System.Drawing.Point(6, 91);
+            this.panel_abm.Name = "panel_abm";
+            this.panel_abm.Size = new System.Drawing.Size(996, 419);
+            this.panel_abm.TabIndex = 109;
+            this.panel_abm.UseWaitCursor = true;
+            // 
+            // btn_promociones
+            // 
+            this.btn_promociones.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(228)))), ((int)(((byte)(137)))), ((int)(((byte)(164)))));
+            this.btn_promociones.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_promociones.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_promociones.Location = new System.Drawing.Point(594, 40);
+            this.btn_promociones.Name = "btn_promociones";
+            this.btn_promociones.Size = new System.Drawing.Size(137, 37);
+            this.btn_promociones.TabIndex = 108;
+            this.btn_promociones.Text = "Promociones";
+            this.btn_promociones.UseVisualStyleBackColor = false;
+            // 
+            // btn_clientes
+            // 
+            this.btn_clientes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(228)))), ((int)(((byte)(137)))), ((int)(((byte)(164)))));
+            this.btn_clientes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_clientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_clientes.Location = new System.Drawing.Point(400, 40);
+            this.btn_clientes.Name = "btn_clientes";
+            this.btn_clientes.Size = new System.Drawing.Size(137, 37);
+            this.btn_clientes.TabIndex = 107;
+            this.btn_clientes.Text = "Clientes";
+            this.btn_clientes.UseVisualStyleBackColor = false;
+            // 
+            // btn_empleados
+            // 
+            this.btn_empleados.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(228)))), ((int)(((byte)(137)))), ((int)(((byte)(164)))));
+            this.btn_empleados.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_empleados.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_empleados.Location = new System.Drawing.Point(202, 40);
+            this.btn_empleados.Name = "btn_empleados";
+            this.btn_empleados.Size = new System.Drawing.Size(137, 37);
+            this.btn_empleados.TabIndex = 106;
+            this.btn_empleados.Text = "Empleados";
+            this.btn_empleados.UseVisualStyleBackColor = false;
+            // 
+            // btn_perfumes
+            // 
+            this.btn_perfumes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(228)))), ((int)(((byte)(137)))), ((int)(((byte)(164)))));
+            this.btn_perfumes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_perfumes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_perfumes.Location = new System.Drawing.Point(6, 40);
+            this.btn_perfumes.Name = "btn_perfumes";
+            this.btn_perfumes.Size = new System.Drawing.Size(137, 37);
+            this.btn_perfumes.TabIndex = 105;
+            this.btn_perfumes.Text = "Perfumes";
+            this.btn_perfumes.UseVisualStyleBackColor = false;
+            // 
+            // MenuABM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(225)))), ((int)(((byte)(230)))));
+            this.ClientSize = new System.Drawing.Size(1008, 548);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel_abm);
+            this.Controls.Add(this.btn_promociones);
+            this.Controls.Add(this.btn_clientes);
+            this.Controls.Add(this.btn_empleados);
+            this.Controls.Add(this.btn_perfumes);
+            this.Name = "MenuABM";
+            this.Text = "MenuABM";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel_abm;
+        private System.Windows.Forms.Button btn_promociones;
+        private System.Windows.Forms.Button btn_clientes;
+        private System.Windows.Forms.Button btn_empleados;
+        private System.Windows.Forms.Button btn_perfumes;
+    }
+}
