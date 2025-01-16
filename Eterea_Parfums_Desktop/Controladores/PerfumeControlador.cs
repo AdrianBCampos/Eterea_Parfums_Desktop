a896752d09db5b34638bdac9a8c53ef3db186eed
joseluisx10 <joseluis.quispe_91@hotmail.com>
2025-01-16 19:09:51 -0300

    edit:  PerfumeControlador getAll()

--- Eterea_Parfums_Desktop/Controladores/PerfumeControlador.cs
+++ Eterea_Parfums_Desktop/Controladores/PerfumeControlador.cs
@@ -41,14 +41,15 @@ namespace Eterea_Parfums_Desktop.Controladores
                     if (r.GetInt32(13) == 1)
                     {
 
+                        string cod1 = r.GetInt64(1).ToString();
 
-                        lista_perfumes.Add(new Perfume(r.GetInt32(0), r.GetInt64(1).ToString(), marca, r.GetString(3),
+                        lista_perfumes.Add(new Perfume(r.GetInt32(0),cod1, marca, r.GetString(3),
                         tipo_de_perfume, genero, r.GetInt32(6), pais,
                         r.GetInt32(8), r.GetInt32(9), r.GetString(10), r.GetInt32(11), r.GetDouble(12),
                         r.GetInt32(13), r.GetString(14), r.GetString(15)));
                     }
 
-                    //Trace.WriteLine("Perfume encontrado, nombre: " + r.GetString(1));
+                    
                 }
                 r.Close();
                 DB_Controller.connection.Close();
