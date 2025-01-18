03ead22ae2d3cd4f663c95f5372b8aa716fda790
DamiMorlan <damian.morlan@davinci.edu.ar>
2025-01-18 17:08:06 -0300

    Arreglando errores del merge que hicimos

--- Eterea_Parfums_Desktop/FormCrearPromo.Designer.cs
+++ Eterea_Parfums_Desktop/FormCrearPromo.Designer.cs
@@ -59,10 +59,9 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(236)))), ((int)(((byte)(239)))));
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.pictureBox1.Location = new System.Drawing.Point(36, 64);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(32, 51);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(374, 391);
+            this.pictureBox1.Size = new System.Drawing.Size(332, 313);
             this.pictureBox1.TabIndex = 100;
             this.pictureBox1.TabStop = false;
             // 
@@ -72,186 +71,194 @@
             this.lbl_crear_promo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(225)))), ((int)(((byte)(230)))));
             this.lbl_crear_promo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_crear_promo.ForeColor = System.Drawing.Color.Black;
-            this.lbl_crear_promo.Location = new System.Drawing.Point(31, 19);
+            this.lbl_crear_promo.Location = new System.Drawing.Point(28, 15);
             this.lbl_crear_promo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_crear_promo.Name = "lbl_crear_promo";
-            this.lbl_crear_promo.Size = new System.Drawing.Size(218, 29);
+            this.lbl_crear_promo.Size = new System.Drawing.Size(174, 24);
             this.lbl_crear_promo.TabIndex = 101;
             this.lbl_crear_promo.Text = "Crear Promoción:";
             // 
             // lbl_tipo_promo
             // 
             this.lbl_tipo_promo.AutoSize = true;
-            this.lbl_tipo_promo.Location = new System.Drawing.Point(44, 78);
+            this.lbl_tipo_promo.Location = new System.Drawing.Point(39, 62);
             this.lbl_tipo_promo.Name = "lbl_tipo_promo";
-            this.lbl_tipo_promo.Size = new System.Drawing.Size(143, 20);
+            this.lbl_tipo_promo.Size = new System.Drawing.Size(124, 16);
             this.lbl_tipo_promo.TabIndex = 102;
             this.lbl_tipo_promo.Text = "Tipo de promoción:";
             // 
             // lbl_nombre_promo
             // 
             this.lbl_nombre_promo.AutoSize = true;
-            this.lbl_nombre_promo.Location = new System.Drawing.Point(44, 134);
+            this.lbl_nombre_promo.Location = new System.Drawing.Point(39, 107);
             this.lbl_nombre_promo.Name = "lbl_nombre_promo";
-            this.lbl_nombre_promo.Size = new System.Drawing.Size(69, 20);
+            this.lbl_nombre_promo.Size = new System.Drawing.Size(59, 16);
             this.lbl_nombre_promo.TabIndex = 103;
             this.lbl_nombre_promo.Text = "Nombre:";
             // 
             // lbl_fecha_ini_promo
             // 
             this.lbl_fecha_ini_promo.AutoSize = true;
-            this.lbl_fecha_ini_promo.Location = new System.Drawing.Point(44, 201);
+            this.lbl_fecha_ini_promo.Location = new System.Drawing.Point(39, 161);
             this.lbl_fecha_ini_promo.Name = "lbl_fecha_ini_promo";
-            this.lbl_fecha_ini_promo.Size = new System.Drawing.Size(119, 20);
+            this.lbl_fecha_ini_promo.Size = new System.Drawing.Size(101, 16);
             this.lbl_fecha_ini_promo.TabIndex = 104;
             this.lbl_fecha_ini_promo.Text = "Fecha de inicio:";
             // 
             // lbl_fecha_fin_promo
             // 
             this.lbl_fecha_fin_promo.AutoSize = true;
-            this.lbl_fecha_fin_promo.Location = new System.Drawing.Point(44, 275);
+            this.lbl_fecha_fin_promo.Location = new System.Drawing.Point(39, 220);
             this.lbl_fecha_fin_promo.Name = "lbl_fecha_fin_promo";
-            this.lbl_fecha_fin_promo.Size = new System.Drawing.Size(162, 20);
+            this.lbl_fecha_fin_promo.Size = new System.Drawing.Size(136, 16);
             this.lbl_fecha_fin_promo.TabIndex = 105;
             this.lbl_fecha_fin_promo.Text = "Fecha de finalización:";
             // 
             // lbl_activo_promo
             // 
             this.lbl_activo_promo.AutoSize = true;
-            this.lbl_activo_promo.Location = new System.Drawing.Point(44, 339);
+            this.lbl_activo_promo.Location = new System.Drawing.Point(39, 271);
             this.lbl_activo_promo.Name = "lbl_activo_promo";
-            this.lbl_activo_promo.Size = new System.Drawing.Size(52, 20);
+            this.lbl_activo_promo.Size = new System.Drawing.Size(44, 16);
             this.lbl_activo_promo.TabIndex = 106;
             this.lbl_activo_promo.Text = "Activo";
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(216, 78);
+            this.comboBox1.Location = new System.Drawing.Point(192, 62);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(180, 28);
+            this.comboBox1.Size = new System.Drawing.Size(160, 24);
             this.comboBox1.TabIndex = 107;
             // 
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(216, 201);
+            this.comboBox2.Location = new System.Drawing.Point(192, 161);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(180, 28);
+            this.comboBox2.Size = new System.Drawing.Size(160, 24);
             this.comboBox2.TabIndex = 108;
             // 
             // comboBox3
             // 
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(216, 267);
+            this.comboBox3.Location = new System.Drawing.Point(192, 214);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(180, 28);
+            this.comboBox3.Size = new System.Drawing.Size(160, 24);
             this.comboBox3.TabIndex = 109;
             // 
             // comboBox4
             // 
             this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Location = new System.Drawing.Point(216, 331);
+            this.comboBox4.Location = new System.Drawing.Point(192, 265);
+            this.comboBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBox4.Name = "comboBox4";
-            this.comboBox4.Size = new System.Drawing.Size(180, 28);
+            this.comboBox4.Size = new System.Drawing.Size(160, 24);
             this.comboBox4.TabIndex = 110;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(216, 134);
+            this.textBox1.Location = new System.Drawing.Point(192, 107);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(180, 26);
+            this.textBox1.Size = new System.Drawing.Size(160, 22);
             this.textBox1.TabIndex = 111;
             // 
             // pictureBox2
             // 
             this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(236)))), ((int)(((byte)(239)))));
             this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.pictureBox2.Location = new System.Drawing.Point(429, 64);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox2.Location = new System.Drawing.Point(381, 51);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(236, 391);
+            this.pictureBox2.Size = new System.Drawing.Size(210, 313);
             this.pictureBox2.TabIndex = 112;
             this.pictureBox2.TabStop = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(439, 78);
+            this.label1.Location = new System.Drawing.Point(390, 62);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(216, 20);
+            this.label1.Size = new System.Drawing.Size(183, 16);
             this.label1.TabIndex = 113;
             this.label1.Text = "Seleccionar producto a incluir";
             // 
             // comboBox5
             // 
             this.comboBox5.FormattingEnabled = true;
-            this.comboBox5.Location = new System.Drawing.Point(459, 134);
+            this.comboBox5.Location = new System.Drawing.Point(408, 107);
+            this.comboBox5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBox5.Name = "comboBox5";
-            this.comboBox5.Size = new System.Drawing.Size(180, 28);
+            this.comboBox5.Size = new System.Drawing.Size(160, 24);
             this.comboBox5.TabIndex = 114;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(514, 218);
+            this.label2.Location = new System.Drawing.Point(457, 174);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(67, 20);
+            this.label2.Size = new System.Drawing.Size(55, 16);
             this.label2.TabIndex = 115;
             this.label2.Text = "Buscar: ";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(459, 255);
+            this.textBox2.Location = new System.Drawing.Point(408, 204);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(180, 26);
+            this.textBox2.Size = new System.Drawing.Size(160, 22);
             this.textBox2.TabIndex = 116;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(455, 316);
+            this.label3.Location = new System.Drawing.Point(404, 253);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(195, 20);
+            this.label3.Size = new System.Drawing.Size(166, 16);
             this.label3.TabIndex = 117;
             this.label3.Text = "Resultado de la busqueda";
             // 
             // btn_agregarProdPromo
             // 
-            this.btn_agregarProdPromo.Location = new System.Drawing.Point(506, 402);
+            this.btn_agregarProdPromo.Location = new System.Drawing.Point(450, 322);
+            this.btn_agregarProdPromo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_agregarProdPromo.Name = "btn_agregarProdPromo";
-            this.btn_agregarProdPromo.Size = new System.Drawing.Size(75, 23);
+            this.btn_agregarProdPromo.Size = new System.Drawing.Size(67, 18);
             this.btn_agregarProdPromo.TabIndex = 118;
             this.btn_agregarProdPromo.Text = "Agregar";
             this.btn_agregarProdPromo.UseVisualStyleBackColor = true;
+
             // 
             // pictureBox3
             // 
             this.pictureBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(236)))), ((int)(((byte)(239)))));
             this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.pictureBox3.Location = new System.Drawing.Point(682, 64);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox3.Location = new System.Drawing.Point(606, 51);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(374, 391);
+            this.pictureBox3.Size = new System.Drawing.Size(332, 313);
             this.pictureBox3.TabIndex = 119;
             this.pictureBox3.TabStop = false;
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(717, 78);
+            this.dataGridView1.Location = new System.Drawing.Point(637, 62);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(292, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(260, 120);
             this.dataGridView1.TabIndex = 120;
             // 
             // FormCrearPromo
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(225)))), ((int)(((byte)(230)))));
-            this.ClientSize = new System.Drawing.Size(1077, 473);
+            this.ClientSize = new System.Drawing.Size(957, 378);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.btn_agregarProdPromo);
@@ -273,6 +280,7 @@
             this.Controls.Add(this.lbl_tipo_promo);
             this.Controls.Add(this.lbl_crear_promo);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormCrearPromo";
             this.Text = "FormCrearPromo";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
