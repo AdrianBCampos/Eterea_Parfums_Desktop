adce3f30db5d2d213c8a0edb8931dfd4a4ca1965
DamiMorlan <damian.morlan@davinci.edu.ar>
2025-04-09 20:02:54 -0300

    Form Star modificado

--- Eterea_Parfums_Desktop/FormStart.cs
+++ Eterea_Parfums_Desktop/FormStart.cs
@@ -13,97 +13,47 @@ namespace Eterea_Parfums_Desktop
     public partial class FormStart : Form
     {
         private BarcodeReceiver barcodeReceiver;
-
-       
         public FormStart()
         {
             InitializeComponent();
 
-            lbl_escala.Visible = false;
-            comboEscala.Visible = false;
-
-            comboEscala.Items.AddRange(new object[] {
-                "100%", "90%", "80%", "70%", "60%"
-            });
-            comboEscala.SelectedItem = "100%"; // Valor por defecto
-            comboEscala.SelectedIndexChanged += ComboEscala_SelectedIndexChanged;
-
-
+           
 
             barcodeReceiver = new BarcodeReceiver();
             barcodeReceiver.StartServer(); // Inicia el servidor TCP
 
             this.KeyPreview = true;
 
-            //this.Load += FormStart_Load; // Suscribir el evento Load al método FormStart_Load
+            this.Load += FormStart_Load; // Suscribir el evento Load al método FormStart_Load
 
         }
 
-        private void ComboEscala_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            string seleccion = comboEscala.SelectedItem.ToString().Replace("%", "");
-            if (float.TryParse(seleccion, out float porcentaje))
-            {
-                Program.ScaleFactor = porcentaje / 100f;
-                MessageBox.Show($"Factor de escala configurado en {Program.ScaleFactor}", "Escala configurada", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-        }
+
+         /*private void InicioAutoConsultas_KeyDown_1(object sender, KeyEventArgs e)
+         {
 
 
+             // Detectar si se presionan las teclas Ctrl + X
+             if (e.Control && e.KeyCode == Keys.X)
+             {
+                 DialogResult result = MessageBox.Show(
+                     "¿Está seguro que desea cerrar la aplicación?",
+                     "Confirmar salida",
+                     MessageBoxButtons.YesNo,
+                     MessageBoxIcon.Question
+                 );
 
+                 if (result == DialogResult.Yes)
+                 {
+                     Application.Exit();
+                 }
+             }
+         }
+         */
 
+        
 
-
-        private void InicioAutoConsultas_KeyDown_1(object sender, KeyEventArgs e)
-        {
-
-
-            // Detectar si se presionan las teclas Ctrl + X
-            if (e.Control && e.KeyCode == Keys.X)
-            {
-                DialogResult result = MessageBox.Show(
-                    "¿Está seguro que desea cerrar la aplicación?",
-                    "Confirmar salida",
-                    MessageBoxButtons.YesNo,
-                    MessageBoxIcon.Question
-                );
-
-                if (result == DialogResult.Yes)
-                {
-                    Application.Exit();
-                }
-            }
-        }
-
-        private void btn_start_Click(object sender, EventArgs e)
-        {
-
-            btn_start.Hide();
-            FormInicioAutoconsulta formInicioAutoconsulta = new FormInicioAutoconsulta();
-            formInicioAutoconsulta.ShowDialog();
-            btn_start.Visible = false;
-            btn_escalar.Visible = false;
-            lbl_escala.Visible = false;
-            comboEscala.Visible = false;
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            btn_escalar.Visible = false;
-            lbl_escala.Visible = true;
-            comboEscala.Visible = true;
-        }
-
-
-
-
-
-
-
-
-
-        /* private void FormStart_Load(object sender, EventArgs e)
+         private void FormStart_Load(object sender, EventArgs e)
           {
               // Llamar al método para abrir FormInicioAutoconsulta automáticamente
               AbrirFormInicioAutoconsulta();
@@ -130,7 +80,7 @@ namespace Eterea_Parfums_Desktop
 
               // **Deshabilitar FormStart para que no reciba clics ni interacciones**
               this.Enabled = false;
-          }*/
+          }
 
 
 
