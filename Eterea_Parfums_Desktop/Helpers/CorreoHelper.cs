0fffa9b55de163dcbd4a52bdace09f0e4b567be3
MaxiKitagawa <maximiliano.kitagawa@davinci.edu.ar>
2025-06-17 17:00:05 -0300

    Fix: enviar factura por mail utilizando el correohelper

--- Eterea_Parfums_Desktop/Helpers/CorreoHelper.cs
+++ Eterea_Parfums_Desktop/Helpers/CorreoHelper.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.IO;
 
 namespace Eterea_Parfums_Desktop.Helpers
 {
@@ -48,6 +50,47 @@ namespace Eterea_Parfums_Desktop.Helpers
                 MessageBox.Show("Error al enviar correo: " + ex.Message);
             }
         }
+        public static void EnviarCorreoFactura(string rutaArchivoPdf, string emailDestino)
+        {
+            string asunto = "Factura de tu compra";
+            string mensaje = "Adjunto encontrarás tu factura en PDF. ¡Gracias por tu compra!";
+
+            try
+            {
+
+                if (!File.Exists(rutaArchivoPdf))
+                {
+                    MessageBox.Show("No se encontró el archivo PDF a adjuntar.");
+                    return;
+                }
+
+                var mail = new MailMessage();
+                mail.From = new MailAddress("etereaparfumsinfo@gmail.com", "Etérea Parfums");
+                mail.To.Add(emailDestino);
+                mail.Subject = asunto;
+                mail.Body = mensaje;
+                mail.IsBodyHtml = false;
+
+                // Adjuntar el PDF
+                if (!string.IsNullOrEmpty(rutaArchivoPdf))
+                {
+                    Attachment archivo = new Attachment(rutaArchivoPdf);
+                    mail.Attachments.Add(archivo);
+                }
+
+                var smtp = new SmtpClient("smtp.gmail.com", 587);
+                smtp.Credentials = new NetworkCredential("etereaparfumsinfo@gmail.com", "jcfy ubtj cknd bosb");
+                smtp.EnableSsl = true;
+
+                smtp.Send(mail);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al enviar la factura: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
     }
 
 }
