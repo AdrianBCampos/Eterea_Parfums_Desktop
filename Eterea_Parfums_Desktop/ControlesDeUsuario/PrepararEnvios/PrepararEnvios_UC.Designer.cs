3fd637fe48f03bb6c8b253f216df83f0a85561e9
AdrianBCampos <adrian.campos@davinci.edu.ar>
2025-08-07 12:14:47 -0300

    ok

--- Eterea_Parfums_Desktop/ControlesDeUsuario/PrepararEnvios/PrepararEnvios_UC.Designer.cs
+++ Eterea_Parfums_Desktop/ControlesDeUsuario/PrepararEnvios/PrepararEnvios_UC.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PrepararEnvios_UC));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.lbl_pedido_buscado = new System.Windows.Forms.Label();
@@ -51,164 +52,98 @@
             // 
             // pictureBox1
             // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
             this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(225)))), ((int)(((byte)(230)))));
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.pictureBox1.Location = new System.Drawing.Point(17, 194);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1979, 432);
-            this.pictureBox1.TabIndex = 319;
             this.pictureBox1.TabStop = false;
             // 
             // flowLayoutPanel1
             // 
-            this.flowLayoutPanel1.AutoScroll = true;
-            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(30, 205);
-            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
+            resources.ApplyResources(this.flowLayoutPanel1, "flowLayoutPanel1");
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(1953, 409);
-            this.flowLayoutPanel1.TabIndex = 327;
-            this.flowLayoutPanel1.WrapContents = false;
             // 
             // lbl_pedido_buscado
             // 
-            this.lbl_pedido_buscado.AutoSize = true;
+            resources.ApplyResources(this.lbl_pedido_buscado, "lbl_pedido_buscado");
             this.lbl_pedido_buscado.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(225)))), ((int)(((byte)(230)))));
-            this.lbl_pedido_buscado.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_pedido_buscado.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(195)))), ((int)(((byte)(156)))), ((int)(((byte)(164)))));
-            this.lbl_pedido_buscado.Location = new System.Drawing.Point(1379, 30);
-            this.lbl_pedido_buscado.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_pedido_buscado.Name = "lbl_pedido_buscado";
-            this.lbl_pedido_buscado.Size = new System.Drawing.Size(222, 32);
-            this.lbl_pedido_buscado.TabIndex = 332;
-            this.lbl_pedido_buscado.Text = "Pedido Filtrado";
             // 
             // lbl_cantidad_ordenes
             // 
-            this.lbl_cantidad_ordenes.AutoSize = true;
+            resources.ApplyResources(this.lbl_cantidad_ordenes, "lbl_cantidad_ordenes");
             this.lbl_cantidad_ordenes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(225)))), ((int)(((byte)(230)))));
-            this.lbl_cantidad_ordenes.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_cantidad_ordenes.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(195)))), ((int)(((byte)(156)))), ((int)(((byte)(164)))));
-            this.lbl_cantidad_ordenes.Location = new System.Drawing.Point(745, 120);
-            this.lbl_cantidad_ordenes.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_cantidad_ordenes.Name = "lbl_cantidad_ordenes";
-            this.lbl_cantidad_ordenes.Size = new System.Drawing.Size(481, 32);
-            this.lbl_cantidad_ordenes.TabIndex = 331;
-            this.lbl_cantidad_ordenes.Text = "Cantidad de ordenes a despachar:";
             // 
             // txt_cantidad_ordenes
             // 
-            this.txt_cantidad_ordenes.AutoSize = true;
+            resources.ApplyResources(this.txt_cantidad_ordenes, "txt_cantidad_ordenes");
             this.txt_cantidad_ordenes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(225)))), ((int)(((byte)(230)))));
-            this.txt_cantidad_ordenes.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txt_cantidad_ordenes.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(195)))), ((int)(((byte)(156)))), ((int)(((byte)(164)))));
-            this.txt_cantidad_ordenes.Location = new System.Drawing.Point(1254, 121);
-            this.txt_cantidad_ordenes.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.txt_cantidad_ordenes.Name = "txt_cantidad_ordenes";
-            this.txt_cantidad_ordenes.Size = new System.Drawing.Size(261, 32);
-            this.txt_cantidad_ordenes.TabIndex = 333;
-            this.txt_cantidad_ordenes.Text = "Cantidad Ordenes";
             // 
             // btn_volver
             // 
+            resources.ApplyResources(this.btn_volver, "btn_volver");
             this.btn_volver.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(228)))), ((int)(((byte)(137)))), ((int)(((byte)(164)))));
-            this.btn_volver.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_volver.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_volver.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(236)))), ((int)(((byte)(239)))));
-            this.btn_volver.Location = new System.Drawing.Point(921, 637);
-            this.btn_volver.Margin = new System.Windows.Forms.Padding(4);
             this.btn_volver.Name = "btn_volver";
-            this.btn_volver.Size = new System.Drawing.Size(173, 45);
-            this.btn_volver.TabIndex = 355;
-            this.btn_volver.Text = "Volver";
             this.btn_volver.UseVisualStyleBackColor = false;
             this.btn_volver.Click += new System.EventHandler(this.btn_volver_Click);
             // 
             // lbl_facturacion
             // 
-            this.lbl_facturacion.AutoSize = true;
+            resources.ApplyResources(this.lbl_facturacion, "lbl_facturacion");
             this.lbl_facturacion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(225)))), ((int)(((byte)(230)))));
-            this.lbl_facturacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_facturacion.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(195)))), ((int)(((byte)(156)))), ((int)(((byte)(164)))));
-            this.lbl_facturacion.Location = new System.Drawing.Point(754, 30);
-            this.lbl_facturacion.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_facturacion.Name = "lbl_facturacion";
-            this.lbl_facturacion.Size = new System.Drawing.Size(461, 38);
-            this.lbl_facturacion.TabIndex = 406;
-            this.lbl_facturacion.Text = "Lista de Envios a Despachar";
             // 
             // pictureBox9
             // 
+            resources.ApplyResources(this.pictureBox9, "pictureBox9");
             this.pictureBox9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(225)))), ((int)(((byte)(230)))));
-            this.pictureBox9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.pictureBox9.Location = new System.Drawing.Point(17, 16);
-            this.pictureBox9.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox9.Name = "pictureBox9";
-            this.pictureBox9.Size = new System.Drawing.Size(1979, 64);
-            this.pictureBox9.TabIndex = 408;
             this.pictureBox9.TabStop = false;
             // 
             // pictureBox7
             // 
+            resources.ApplyResources(this.pictureBox7, "pictureBox7");
             this.pictureBox7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(167)))), ((int)(((byte)(180)))));
-            this.pictureBox7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.pictureBox7.Location = new System.Drawing.Point(7, 9);
-            this.pictureBox7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox7.Name = "pictureBox7";
-            this.pictureBox7.Size = new System.Drawing.Size(1999, 79);
-            this.pictureBox7.TabIndex = 407;
             this.pictureBox7.TabStop = false;
             // 
             // pictureBox6
             // 
+            resources.ApplyResources(this.pictureBox6, "pictureBox6");
             this.pictureBox6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(225)))), ((int)(((byte)(230)))));
-            this.pictureBox6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.pictureBox6.Location = new System.Drawing.Point(17, 103);
-            this.pictureBox6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(1979, 64);
-            this.pictureBox6.TabIndex = 412;
             this.pictureBox6.TabStop = false;
             // 
             // pictureBox4
             // 
+            resources.ApplyResources(this.pictureBox4, "pictureBox4");
             this.pictureBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(167)))), ((int)(((byte)(180)))));
-            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.pictureBox4.Location = new System.Drawing.Point(7, 96);
-            this.pictureBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(1999, 79);
-            this.pictureBox4.TabIndex = 411;
             this.pictureBox4.TabStop = false;
             // 
             // txt_nombre_empleado
             // 
-            this.txt_nombre_empleado.AutoSize = true;
+            resources.ApplyResources(this.txt_nombre_empleado, "txt_nombre_empleado");
             this.txt_nombre_empleado.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(225)))), ((int)(((byte)(230)))));
-            this.txt_nombre_empleado.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txt_nombre_empleado.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(195)))), ((int)(((byte)(156)))), ((int)(((byte)(164)))));
-            this.txt_nombre_empleado.Location = new System.Drawing.Point(69, 121);
-            this.txt_nombre_empleado.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.txt_nombre_empleado.Name = "txt_nombre_empleado";
-            this.txt_nombre_empleado.Size = new System.Drawing.Size(300, 31);
-            this.txt_nombre_empleado.TabIndex = 413;
-            this.txt_nombre_empleado.Text = "Nombre del Empleado";
             // 
             // pictureBox2
             // 
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
             this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(167)))), ((int)(((byte)(180)))));
-            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.pictureBox2.Location = new System.Drawing.Point(7, 183);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(1999, 509);
-            this.pictureBox2.TabIndex = 414;
             this.pictureBox2.TabStop = false;
             // 
             // PrepararEnvios_UC
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(186)))), ((int)(((byte)(197)))));
             this.Controls.Add(this.btn_volver);
@@ -224,9 +159,7 @@
             this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.pictureBox2);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "PrepararEnvios_UC";
-            this.Size = new System.Drawing.Size(2015, 700);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
